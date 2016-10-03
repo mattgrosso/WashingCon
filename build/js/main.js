efa2d623738a90cdfd848ff0e02ff8d0e1bca468
MJG <MJG@MacBook-Pro.local>
2016-10-03 13:56:16 -0400

    Adding various page templates and ui-router structure.

--- build/js/main.js
+++ build/js/main.js
@@ -23,6 +23,42 @@
         templateUrl: 'getinvolved/getinvolved.template.html',
         controller: 'GetInvolvedController',
         controllerAs: 'getinvolved'
+      })
+      .state('sponsors', {
+        url: '/sponsors',
+        templateUrl: 'sponsors/sponsors.template.html',
+        controller: 'SponsorsController',
+        controllerAs: 'sponsors'
+      })
+      .state('contact', {
+        url: '/contact',
+        templateUrl: 'contact/contact.template.html',
+        controller: 'ContactController',
+        controllerAs: 'contact'
+      })
+      .state('about', {
+        url: '/about',
+        templateUrl: 'about/about.template.html',
+        controller: 'AboutController',
+        controllerAs: 'about'
+      })
+      .state('location', {
+        url: '/location',
+        templateUrl: 'location/location.template.html',
+        controller: 'LocationController',
+        controllerAs: 'location'
+      })
+      .state('registration', {
+        url: '/registration',
+        templateUrl: 'registration/registration.template.html',
+        controller: 'RegistrationController',
+        controllerAs: 'registration'
+      })
+      .state('events', {
+        url: '/events',
+        templateUrl: 'events/events.template.html',
+        controller: 'EventsController',
+        controllerAs: 'events'
       });
   }
 
