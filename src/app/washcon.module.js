2943f17905fdbd957857b9ed8d73d800760b0a16
MJG <MJG@MacBook-Pro.local>
2016-10-03 13:36:38 -0400

    Just getting some basic structure set up

--- src/app/washcon.module.js
+++ src/app/washcon.module.js
@@ -8,8 +8,22 @@
   washconConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
 
   function washconConfig($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise('/');
 
+    $urlRouterProvider.otherwise('/home');
+
+    $stateProvider
+      .state('home', {
+        url: '/home',
+        templateUrl: 'home/home.template.html',
+        controller: 'HomeController',
+        controllerAs: 'home'
+      })
+      .state('getinvolved', {
+        url: '/getinvolved',
+        templateUrl: 'getinvolved/getinvolved.template.html',
+        controller: 'GetInvolvedController',
+        controllerAs: 'getinvolved'
+      });
   }
 
 })();
